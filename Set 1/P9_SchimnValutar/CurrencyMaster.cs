d201715261a2137b92ba50021445fad1b8ee7191
Asmarandei Catalin <asmarandei.catalin@yahoo.com>
2025-10-15 14:00:38 +0300

    [Set1-P9] done

---
+++ Set 1/P9_SchimnValutar/CurrencyMaster.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P9_Schimb_Valutar
+{
+    public class CurrencyMaster
+    {
+        private bool _running = true;
+        private readonly Dictionary<DateTime, Dictionary<CurrencyData.Currency, decimal>> _data;
+
+        public CurrencyMaster()
+        {
+            _data = CurrencyData.GetData();
+            Main();
+        }
+
+        private void Main()
+        {
+            Console.WriteLine("Schimb Valutar is running(- to stop it)");
+            while (_running)
+            {
+                if (!TakeInput("money", out var input))
+                    continue;
+
+                if (input == "-")
+                {
+                    _running = false;
+                    continue;
+                }
+
+                if (!decimal.TryParse(input, out var money))
+                {
+                    Console.WriteLine("Enter valid money!");
+                    continue;
+                }
+
+                Console.WriteLine($"Currency table for {money}RON");
+
+                var today = DateTime.Today;
+                var tblSpace = $"{DateFormat(today)}".Length + 3;
+                // var tblSpace = $"{_data[DateTime.Today][CurrencyData.Currency.Eur] * money}".Length + 3;
+                var tbl = new Table(tblSpace);
+
+                tbl.PrintCell("Date/Curreny");
+                // Enumerable.Range(0, 30).Select(i => DateTime.Today.AddDays(-i)).ToList().ForEach(type => tbl.PrintCell(type));
+                Enum.GetNames(typeof(CurrencyData.Currency)).ToList().ForEach(type => tbl.PrintCell(type));
+                tbl.EndLine();
+
+
+                for (var i = 0; i < 30; i++)
+                {
+                    var date = today.AddDays(-i);
+                    tbl.PrintCell(DateFormat(date));
+                    _data[date].Values.ToList().ForEach(val => tbl.PrintCell($"{money / val:0.00}"));
+                    tbl.EndLine();
+                }
+            }
+        }
+
+        private string DateFormat(DateTime date) => date.ToShortDateString();
+
+        private bool TakeInput(string param, out string input)
+        {
+            Console.WriteLine($"Enter a {param}: ");
+            var userInput = Console.ReadLine();
+
+            if (String.IsNullOrWhiteSpace(userInput))
+            {
+                Console.WriteLine($"Please enter a valid {param}!");
+                input = string.Empty;
+                return false;
+            }
+
+            input = userInput;
+            return true;
+        }
+    }
+}
---
+++ Set 1/P9_SchimnValutar/Table.cs
@@ -0,0 +1,15 @@
+﻿namespace P9_Schimb_Valutar
+{
+    public class Table
+    {
+        public int TblPadding { get; }
+
+        public Table(int tblPadding)
+        {
+            this.TblPadding = tblPadding;
+        }
+
+        public void PrintCell(object value) => Console.Write("|" + $"{value}".PadLeft(TblPadding));
+        public void EndLine() => Console.WriteLine("|");
+    }
+}
