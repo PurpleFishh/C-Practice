95b64c4aa40572ece32462a7900459c3d0ef4164
Asmarandei Catalin <asmarandei.catalin@yahoo.com>
2025-10-15 13:51:22 +0300

    [Set1-P2] done

---
+++ Set 1/P2_Note/Catalog.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_Note
+{
+    public class Catalog
+    {
+        private bool _running = true;
+        private Dictionary<string, List<double>> lectureGrades = new();
+
+        public void Start()
+        {
+            Console.WriteLine("Enter the grade, when done enter -");
+            while (_running)
+            {
+                Console.Write("Enter lecture: ");
+                var lecture = Console.ReadLine();
+                if (String.IsNullOrWhiteSpace(lecture))
+                {
+                    Console.WriteLine("Please enter a valid lecture name!");
+                    continue;
+                }
+                if (lecture == "-")
+                {
+                    GradesDisplay(lectureGrades);
+                    _running = false;
+                    continue;
+                }
+
+                if (!lectureGrades.ContainsKey(lecture))
+                    lectureGrades[lecture] = new List<double>();
+
+                Console.WriteLine($"Enter grades for lecture {lecture} (- to stop)");
+                bool gradesRecording = true;
+                while (gradesRecording)
+                {
+                    Console.Write($"Grade {lectureGrades[lecture].Count + 1}: ");
+                    var input = Console.ReadLine() ?? "";
+                    if (input == "-")
+                        gradesRecording = false;
+
+                    var grade = ValidateGrade(input);
+                    if(grade.HasValue)
+                        lectureGrades[lecture].Add(grade.Value);
+                }
+            }
+        }
+
+        private double? ValidateGrade(string input)
+        {
+            if (!double.TryParse(input, NumberStyles.Float, CultureInfo.CurrentCulture, out var grade))
+            {
+                Console.WriteLine("Enter a valid grade");
+                return null;
+            }
+            if (grade < 0 || grade > 100)
+            {
+                Console.WriteLine("Please enter a grade between 0.0 and 100.0");
+                return null;
+            }
+            return grade;
+        }
+
+        private void GradesDisplay(Dictionary<string, List<double>> lectureGrades)
+        {
+            if (lectureGrades.Count == 0)
+                Console.WriteLine("No lecture recorded...");
+            foreach (var grades in lectureGrades)
+            {
+                var finalGrade = grades.Value.Sum(x => x) / grades.Value.Count;
+                Console.WriteLine($"Final grade for {grades.Key}: {finalGrade}");
+            }
+        }
+    }
+}
---
+++ Set 1/P2_Note/Program.cs
@@ -0,0 +1,4 @@
+﻿using P2_Note;
+
+Catalog catalog = new Catalog();
+catalog.Start();
