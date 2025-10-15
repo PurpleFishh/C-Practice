315a4b1f0e01e4d6448327ac9e6cd9dca0e40954
Asmarandei Catalin <asmarandei.catalin@yahoo.com>
2025-10-15 13:58:43 +0300

    [Set1-P6] done

---
+++ Set 1/P6_Inmultire/Table.cs
@@ -0,0 +1,15 @@
+﻿namespace P6_Inmultire
+{
+    public class Table
+    {
+        public int TblPadding { get; }
+
+        public Table(int tblPadding)
+        {
+            this.TblPadding = tblPadding;
+        }
+
+        public void PrintCell(object value) => Console.Write("|" + $"{value}".PadLeft(TblPadding));
+        public void EndLine() => Console.WriteLine("|");
+    }
+}
