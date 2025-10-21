e03019179f175c6f5d05630fcbeed976c4a03903
Asmarandei Catalin <asmarandei.catalin@yahoo.com>
2025-10-21 10:55:17 +0300

    [Set1-P2] display and business logic separation

--- Set 1/P2_Note/Catalog.cs
+++ Set 1/P2_Note/Catalog.cs
@@ -8,72 +8,107 @@ using System.Text;
 using System.Threading.Tasks;
 using P2_Note.Grader;
 
-namespace P2_Note
+namespace P2_Note;
+
+public enum CatalogActions
 {
-    public class Catalog
+    AddGrade,
+    ShowSummary,
+    Exit
+}
+
+public class Catalog
+{
+    private bool _running = true;
+    private readonly CatalogController _controller = new();
+
+    public void Start()
     {
-        private bool _running = true;
-        private readonly IGrader _lectureGrades = new LectureGrades();
-
-        public void Start()
+        Console.WriteLine("Catalog started (a - add grades to lecture, m - show summary, - to exit)");
+        while (_running)
         {
-            Console.WriteLine("Enter the grade, when done enter -");
-            while (_running)
+            if (!TakeInput("operation", out var operationInput))
+                continue;
+            try
             {
-                if (!TakeInput("lecture", out var lecture))
-                    continue;
-
-                if (lecture == "-")
+                var action = GetAction(operationInput);
+                switch (action)
                 {
-                    Console.WriteLine(_lectureGrades);
-                    _running = false;
-                    continue;
-                }
-
-                Console.WriteLine($"Enter grades for lecture {lecture} (- to stop)");
-                while (true)
-                {
-                    var gradesCount = _lectureGrades.GetGrades(lecture).Count;
-                    if (!TakeInput($"grade {gradesCount + 1}", out var input))
+                    case CatalogActions.ShowSummary:
+                        Console.WriteLine(_controller.GetSummary());
                         continue;
-                    if (input == "-")
-                        break;
-
-                    if (!ValidateGrade(input, out var grade)) continue;
-                    try
-                    {
-                        _lectureGrades.AddGrade(lecture, grade);
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine(ex.Message);
-                    }
+                    case CatalogActions.AddGrade:
+                        if (!TakeInput("lecture", out var lecture))
+                            continue;
+                        AddGrades(lecture);
+                        continue;
+                    case CatalogActions.Exit:
+                        StopProgram();
+                        continue;
+                    default:
+                        throw new InvalidOperationException("Invalid catalog action");
                 }
             }
-        }
-
-        private bool ValidateGrade(string input, out double grade)
-        {
-            if (double.TryParse(input, out grade)) return true;
-
-            Console.WriteLine("Enter a valid grade");
-            return false;
-        }
-
-        private bool TakeInput(string param, out string input)
-        {
-            Console.WriteLine($"Enter a {param}: ");
-            var userInput = Console.ReadLine();
-
-            if (string.IsNullOrWhiteSpace(userInput))
+            catch (InvalidOperationException ex)
             {
-                Console.WriteLine($"Please enter a valid {param}!");
-                input = string.Empty;
-                return false;
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        Console.WriteLine(_controller.GetSummary());
+    }
+
+    private void AddGrades(string lecture)
+    {
+        Console.WriteLine($"Enter grades for lecture '{lecture}' (enter '-' to stop adding grades)");
+        while (true)
+        {
+            var nextIndex = _controller.GetGrades(lecture).Count + 1;
+            if (!TakeInput($"grade {nextIndex}", out var input, false))
+                break;
+            
+            if (!_controller.TryAddGrade(lecture, input, out var message))
+            {
+                Console.WriteLine($"ERROR: {message}");
+                continue;
             }
 
-            input = userInput;
-            return true;
+            Console.WriteLine(message);
         }
+    }
+
+    private CatalogActions GetAction(string input)
+    {
+        return input.ToLower() switch
+        {
+            "a" => CatalogActions.AddGrade,
+            "m" => CatalogActions.ShowSummary,
+            "-" => CatalogActions.Exit,
+            _ => throw new InvalidOperationException("Invalid catalog action.")
+        };
+    }
+
+
+    private bool TakeInput(string variable, out string input, bool stopProgram = true)
+    {
+        if (!InputHelper.TakeInput(variable, out input))
+            return false;
+
+        if (CheckProgramEnd(input, stopProgram))
+            return false;
+        return true;
+    }
+
+    private bool CheckProgramEnd(string input, bool stopProgram)
+    {
+        if (input != "-") return false;
+        if (stopProgram)
+            StopProgram();
+        return true;
+    }
+
+    private void StopProgram()
+    {
+        _running = false;
     }
 }
