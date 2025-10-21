c29f5baea8c244e0d7b489a264efca6712c1e65a
Asmarandei Catalin <asmarandei.catalin@yahoo.com>
2025-10-21 12:19:49 +0300

    [Set1-P15] done

---
+++ Set 1/P15_Magazin/ShopLibrary/Service/IShopService.cs
@@ -0,0 +1,12 @@
+﻿using P15_Magazin.ShopLibrary.Domain;
+
+namespace P15_Magazin.ShopLibrary.Service;
+
+public interface IShopService
+{
+    public Product AddProduct(string name, decimal price, int stock);
+    public Product? BuyProduct(string name, int quantity);
+    public bool RemoveProduct(string name);
+    public IEnumerable<Product> GetProducts();
+    public decimal GetShopStockValue();
+}
